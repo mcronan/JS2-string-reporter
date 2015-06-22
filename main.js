ab92f9de420e1dfa547c855b8f4f64bc97a57915
Ronan Mc Guire <ronanir600@gmail.com>
2015-06-22 15:28:33 -0600

    reporting strings

---
+++ main.js
@@ -0,0 +1,4 @@
+var greeting = prompt("Please enter any word");
+var greetingLength = greeting.length;
+var response = alert(greeting.toUpperCase() + " is indeed the word you have enered! " + greeting.toUpperCase() + " is " + greetingLength + " letters long!" );
+
