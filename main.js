7b8d00ca3c4c39405018db65dd8be8c445a9d67b
Ronan Mc Guire <ronanir600@gmail.com>
2015-06-22 16:34:46 -0600

    strings

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var greeting = prompt("Please enter any word");
 var greetingLength = greeting.length;
-var response = alert(greeting.toUpperCase() + " is indeed the word you have enered! " + greeting.toUpperCase() + " is " + greetingLength + " letters long!" );
+var greetingChar = greeting.charAt(2);
+var response = alert(greeting.toUpperCase() + " is indeed the word you have enered!" + "\n" + greeting.toLowerCase() + " is " + greetingLength + " letters long!" + "\n" + "The third character is '" + greetingChar  + "'." + "\n" + "Would you like to hear " + greeting + " in a sentecence? Here we go: 'The reason you picked " + greeting + " must be that it is your favourite word!'" + "\n" + "Subword: " + greeting.substring(0,3) );
 
